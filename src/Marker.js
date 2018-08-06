cd01a6d611769c8df2b76219328e4a98c2750588
karolinaseidlova@gmail.com <35819148+BcKarolinaSeidlova@users.noreply.github.com>
2018-08-07 01:34:48 +0200

    add wikipedia fetch

--- src/Marker.js
+++ src/Marker.js
@@ -5,14 +5,15 @@ import './index.css';
 class Marker extends Component {
 
     openMarker = () => {
-        const {tips, map, infoWindow, marker} = this.props;
+        const {map, infoWindow, marker} = this.props;
         map.panTo(marker.getPosition());
         infoWindow.setContent(
             `<div tabIndex="1" name=${ marker.title }>
-                <p>${marker.title}</p>
+                 <h3>${marker.title}</h3>
+                <p> ${marker.info} </p>
             </div>`
         );
-        marker.setAnimation(window.google.maps.Animation.DROP);
+        marker.setAnimation(window.google.maps.Animation.BOUNCE);
         setTimeout(() => {
             marker.setAnimation(null)
         }, 800)
